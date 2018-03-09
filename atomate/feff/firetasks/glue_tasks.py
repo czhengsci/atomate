48cd8f9359e922b35a1636f754a67bd7c7c0cdbc
czhengsci <czhengsci@gmail.com>
2018-03-08 18:34:01 -0800

    modified DbQueryModuleOutputs

--- atomate/feff/firetasks/glue_tasks.py
+++ atomate/feff/firetasks/glue_tasks.py
@@ -3,18 +3,22 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
+from matgendb.util import get_database
 from fireworks import explicit_serialize
 from fireworks import FiretaskBase, FWAction
 from atomate.utils.utils import get_logger
 from atomate.common.firetasks.glue_tasks import get_calc_loc, CopyFiles
-from fireworks.user_objects.firetasks.filepad_tasks import get_fpad
 import six
+import os
+import subprocess
+import re
 
 __author__ = 'Kiran Mathew, Chen Zheng'
 __email__ = 'kmathew@lbl.gov, chz022@ucsd.edu'
 
 logging.basicConfig(filename="feff_run.log", level=logging.INFO)
 logger = get_logger(__name__)
+
 
 @explicit_serialize
 class CopyFeffOutputs(CopyFiles):
@@ -34,7 +38,6 @@ class CopyFeffOutputs(CopyFiles):
     optional_params = ["calc_loc", "calc_dir", "filesystem", "exclude_files"]
 
     def run_task(self, fw_spec):
-
         calc_loc = get_calc_loc(self["calc_loc"], fw_spec["calc_locs"]) if self.get("calc_loc") else {}
         exclude_files = self.get("exclude_files", [])
 
@@ -52,9 +55,12 @@ class DbQueryModuleOutputs(FiretaskBase):
         query_outputs (list): List of outputs files query from gridfs using filepad
         output_identifier (str/list): Identifier label(s) to tag the query module output
                                     files.
+        index_collection (str): Name of the collection in Mongodb stores information, i.e calculation parameters,
+                                file pathways, etc., of calculated intermediate file..
+        indexdb_settings (str): File path to json file used for index database connection setup
+
 
     Optional_params:
-        filepad_file (str): path to the filepad connection settings file.
         metaquery (dict): Other pymongo query conditions in dict format
         calc_inputparams (dict): Input parameters of calculation used for query results filtering
         calc_inputparams_nest (str/list): Nested fields of calculation input parameters.
@@ -66,12 +72,14 @@ class DbQueryModuleOutputs(FiretaskBase):
     def run_task(self, fw_spec):
         query_outputs = self["query_outputs"]
         output_labels = self["output_identifier"]
-        fpad = get_fpad(self.get("filepad_file", None))
+        index_collection = self["index_collection"]
+        indexdb_settings = self["indexdb_settings"]
         metaquery = self.get("metaquery", dict())
 
-        #Process the input parameters and make them into pymongo nested fields for query purpose
+        # Process the input parameters and make them into pymongo nested fields for query purpose
         calc_inputparams = self.get("calc_inputparams", None)
         calc_inputparams_nest = self.get("calc_inputparams_nest", None)
+
         if calc_inputparams and calc_inputparams_nest:
             for k, v in calc_inputparams.items():
                 if isinstance(calc_inputparams_nest, six.string_types):
@@ -84,35 +92,30 @@ class DbQueryModuleOutputs(FiretaskBase):
 
         logger.info("metaquery information is {}".format(metaquery))
 
-        query_identifiers = []
-        fw_mod_spec = {'_push_all':dict()}
+        fw_mod_spec = {'_push_all': dict()}
+        index_db_connection = get_database(indexdb_settings)
+        identifier_pattern = re.compile("{}.*".format(output_labels))
+        db_query_cond = {**{"identifier": identifier_pattern}, **metaquery}
+        cal_cursor = index_db_connection[index_collection].find(db_query_cond)
 
-        for index, output in enumerate(query_outputs):
-            query_file = dict()
-            query_file["filename"] = output
-            query_file["conditions"] = dict()
-            if isinstance(output_labels, six.string_types):
-                query_file["conditions"]["identifier"] = "-".join((output_labels, output))
-            elif isinstance(output_labels, (list,)):
-                query_file["conditions"]["identifier"] = output_labels[index]
+        if cal_cursor.count() > 0:
+            intermediate_files_field = index_db_connection[index_collection].find_one(db_query_cond,
+                                                                                      {"intermediate_files": 1})
 
-            if metaquery:
-                query_file["conditions"] = {**query_file["conditions"], **metaquery}
-
-            query_identifiers.append(query_file)
-
-        for sub_query in query_identifiers:
-            query_results = fpad.get_file_by_query(sub_query["conditions"])
-            if len(query_results)>=0:
-                with open(sub_query["filename"],"wb") as f:
-                    f.write(query_results[0][0])
-                output_name_pseudo = sub_query["filename"].replace('.', "-")
-                fw_mod_spec['_push_all'][output_name_pseudo] = True
-
-        logger.info("Output files queried from database: {}".format(fw_mod_spec['_push_all']))
+            for output_dict in intermediate_files_field["intermediate_files"]:
+                if output_dict["original_file_name"] in query_outputs:
+                    rclone_sync_command = ["rclone", "sync", output_dict["file_storage_path"], os.getcwd()]
+                    return_code = subprocess.call(rclone_sync_command)
+                    output_name_pseudo = output_dict["original_file_name"].replace(".", "-")
+                    fw_mod_spec["_push_all"][output_name_pseudo] = True
+                    logger.info(
+                        "{} queried from database with return code {}.".format(output_dict["original_file_name"],
+                                                                               return_code))
+        elif cal_cursor.count() == 0:
+            logger.info(
+                "For output files {}, with identifier {} and metaquery {}. No matching file could be found.".format(
+                    query_outputs, output_labels, metaquery))
 
         if fw_mod_spec['_push_all']:
             return FWAction(mod_spec=[fw_mod_spec])
-
-
 
